1b2c296c05289fdcb08e5c3c37399419c72cf656
Terin Sebastin <terin01@gmail.com>
2022-04-22 07:05:15 +0530

    Added Urilify Progran

---
+++ DataStructAndAlgos/ArraysAndStrings/Urilify.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructAndAlgos.ArraysAndStrings
+{
+    public class Urilify
+    {
+       public  void ReplaceSpace(char[] str, int length)
+        {
+            int spaceCount = 0, newLength, i;
+            for(i =0; i < length; i++)
+            {
+                if (str[i] ==  ' ')
+                {
+                    spaceCount++;
+                }
+            }
+            newLength = length + spaceCount * 2;
+            str[newLength] = '\0';
+            for (i = length - 1; i >= 0; i--){
+                if(str[i] == ' ')
+                {
+                    str[newLength - 1] = '0';
+                    str[newLength - 2] = '2';
+                    str[newLength - 3] = '%';
+                    newLength = newLength - 3;
+                }
+                else
+                {
+                    str[newLength - 1] = str[i];
+                    newLength = newLength - 1;
+                }
+            }
+        }
+    }
+}
--- DataStructAndAlgos/Program.cs
+++ DataStructAndAlgos/Program.cs
@@ -15,14 +15,18 @@ namespace DataStructAndAlgos
             //objProg.isUniqueCharsMthd2("abcdefghid");
             //Console.WriteLine("Hello World!");
 
-            //2.2 Given two strings write a method to decide if one is a permutation of the other.
+            //1.2 Given two strings write a method to decide if one is a permutation of the other.
             //A Permutation of a string is another string that contains same characters,
             //only the order of characters can be different. For example, “abcd” and “dabc” are Permutation of each other.
             //a) Method 1
-            StringPermutation obj = new StringPermutation();
+            // StringPermutation obj = new StringPermutation();
             //obj.permutation("abcd", "badc");
             //b) Method 2
-            obj.permutation2("abcd", "badc");
+            //obj.permutation2("abcd", "badc");
+
+            //1.3 URLify
+            Urilify objMeth = new Urilify();
+            objMeth.ReplaceSpace("Mr John Smith     ".ToCharArray(), "Mr John Smith     ".Trim().Length);
         }
     }
 }
